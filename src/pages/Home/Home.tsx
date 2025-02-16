702c6473d26918ea4ee56b15075e2c772435c962
Vladislav <dreamervt8@gmail.com>
2025-02-16 15:08:39 +0500

    update npm packages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
 import { Box } from "@mui/material";
-import TestSvg from "../../../public/test.svg";
 
 export const Home = () => {
   return (
     <Box component="section" data-testid="pageHome">
       Home page
-      <Box component="img" src={TestSvg} />
     </Box>
   );
 };
